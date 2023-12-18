490a9cf2061072b54e708cf83fd31988b5f4f74a
Rob Godfrey <rob_godfrey@outlook.com>
2023-12-18 00:26:57 -1000

    Basic input layout

--- main.js
+++ main.js
@@ -1,24 +1,65 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import hint from '/hint.jpg'
+
+const topKeys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
+const middleKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
+const bottomKeys = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+
+const guessChars = [];
+
+function refreshGuesses() {
+    document.querySelectorAll('.guess-box').forEach((box, index) => {
+        box.innerHTML = guessChars[index] || '';
+    });
+}
+
+function pushKey(key) {
+    if (guessChars.length >= 6) return;
+    guessChars.push(key);
+    refreshGuesses();
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <h2>GUESSES REMAINING: 3</h2>
+    <p class="read-the-docs">You can submit 3 guesses every 24 hours</p>
+    <!-- <img src="${hint}" alt="Hint" style="width: 50%; border-radius: 15px;" /> -->
+    <div class="guess-row">
+        <div class="guess-box"> </div>
+        <div class="guess-box"> </div>
+        <div class="guess-box"> </div>
+        <div class="guess-box"> </div>
+        <div class="guess-box"> </div>
+        <div class="guess-box"> </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <div id="keyboard">
+        <div class="row">
+            ${topKeys.map(key => `<button class="key">${key}</button>`).join('')}
+        </div>
+        <div class="row">
+            ${middleKeys.map(key => `<button class="key">${key}</button>`).join('')}
+        </div>
+        <div class="row">
+            <button id="enterKey">ENTER</button>
+            ${bottomKeys.map(key => `<button class="key">${key}</button>`).join('')}
+            <button id="backKey">BACK</button>
+        </div>
+    </div>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+    key.addEventListener('click', () => {
+        pushKey(key.innerHTML);
+    });
+});
+
+document.querySelector('#enterKey').addEventListener('click', () => {
+    console.log('ENTER');
+});
+
+document.querySelector('#backKey').addEventListener('click', () => {
+    guessChars.pop();
+    refreshGuesses();
+});
