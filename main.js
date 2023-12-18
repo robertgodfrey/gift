6fd843051a41c2be8de5a29e7c70e5d165c80954
Rob Godfrey <rob_godfrey@outlook.com>
2023-12-18 00:28:25 -1000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import hint from '/hint.jpg'
+//import hint from '/hint.jpg'
 
 const topKeys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
 const middleKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
