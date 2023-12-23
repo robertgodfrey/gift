27f96f5de196f239d9b43607705201d32559b356
Rob Godfrey <rob_godfrey@outlook.com>
2023-12-22 22:31:03 -1000

    hint 1, modal, loader

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const middleKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
 const bottomKeys = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 const guessChars = [];
 
+const audioHint = new Audio('/hint1.mp3');
+let countdownInterval;
+
 function refreshGuesses() {
     document.querySelectorAll('.guess-box').forEach((box, index) => {
         box.innerHTML = guessChars[index] || '';
@@ -18,9 +21,14 @@ function pushKey(key) {
     refreshGuesses();
 }
 
-function playHint() {
-    const audio = new Audio('/hint.mp3');
-    audio.play();
+function setTimeHint(secondsRemaining) {
+    const formattedSeconds = secondsRemaining < 10 ? `0${secondsRemaining}` : secondsRemaining;
+    document.querySelector('#hintOne').innerHTML = `STOP 0:${formattedSeconds}`;
+}
+
+function showModal() {
+    document.querySelector('#modal').classList.remove('overlay-bg-hidden');
+    document.querySelector('#modal').classList.add('overlay-bg');
 }
 
 document.querySelector('#app').innerHTML = `
@@ -28,7 +36,7 @@ document.querySelector('#app').innerHTML = `
     <h2>GUESS THE CODE</h2>
     <p class="read-the-docs">You can submit a guess every two minutes.<br/>A new hint will appear in 5 guesses.</p>
     <!-- <img src="" alt="Hint" style="width: 50%; border-radius: 15px;" /> -->
-    <button class="hint-btn" onclick="${playHint}">HINT #1</button>
+    <button id="hintOne" class="hint-btn">HINT #1</button>
     <div class="guess-row">
         <div class="guess-box"> </div>
         <div class="guess-box"> </div>
@@ -60,19 +68,30 @@ document.querySelectorAll('.keyboard-key').forEach(key => {
 });
 
 document.querySelector('#enterKey').addEventListener('click', async () => {
-    const res = await fetch('/api/check-code', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ checkCode: guessChars.join('') })
-    });
-    const json = await res.json();
-    if (json.secondsRemaining && json.secondsRemaining > 0) {
-
-    } else {
-        guessChars.length = 0;
-        refreshGuesses();
+    const loading = document.querySelector('#loading');
+    const modal = document.querySelector('#modal');
+    loading.classList.remove('overlay-bg-hidden');
+    loading.classList.add('overlay-bg');
+    try {
+        const res = await fetch('/api/check-code', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ checkCode: guessChars.join('') })
+        });
+        const json = await res.json();
+        if (json.secondsRemaining && json.secondsRemaining > 0) {
+            // modal - ya gotta wait
+        } else {
+            // modal - show guess result
+            guessChars.length = 0;
+            refreshGuesses();
+        }
+    } catch (e) {
+        // modal - error
+        console.error(e);
     }
-    console.log(json);
+    loading.classList.remove('overlay-bg');
+    loading.classList.add('overlay-bg-hidden');
 });
 
 document.querySelector('#backKey').addEventListener('click', () => {
@@ -88,5 +107,36 @@ document.addEventListener('keydown', e => {
         document.querySelector('#backKey').click();
     } else if (e.key.length === 1) {
         pushKey(e.key.toUpperCase());
+    }
+});
+
+document.querySelector('#modal').addEventListener('click', () => {
+    document.querySelector('#modal').classList.remove('overlay-bg');
+    document.querySelector('#modal').classList.add('overlay-bg-hidden');
+});
+
+document.querySelector('#hintOne').addEventListener('click', () => {
+    let secondsRemaining = 27;
+    try {
+        if (!audioHint.paused) {
+            audioHint.pause();
+            audioHint.currentTime = 0;
+            document.querySelector('#hintOne').innerHTML = 'HINT #1';
+            clearInterval(countdownInterval);
+        } else {
+            audioHint.play();
+            countdownInterval = setInterval(() => {
+                setTimeHint(secondsRemaining);
+                secondsRemaining--;
+                // clear interval if seconds below 1
+                if (secondsRemaining < 0) {
+                    clearInterval(countdownInterval);
+                    document.querySelector('#hintOne').innerHTML = 'HINT #1';
+                }
+            }, 1000);
+            document.querySelector('#hintOne').innerHTML = 'STOP 0:28';
+        }
+    } catch (e) {
+        console.error(e);
     }
 });
